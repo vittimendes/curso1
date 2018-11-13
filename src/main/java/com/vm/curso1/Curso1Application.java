74253cc8fbc7ed35d55349126d41b2b8c77d60b2
vittimendes <vitormendes@gmail.com>
2018-11-13 00:47:03 -0200

    Criação Projeto

---
+++ src/main/java/com/vm/curso1/Curso1Application.java
@@ -0,0 +1,12 @@
+package com.vm.curso1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curso1Application.class, args);
+	}
+}
